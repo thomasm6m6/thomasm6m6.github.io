4019c514cfa84d64e65767cc8ed01735279d5ed4
Thomas M <thomasm6m6@gmail.com>
2025-01-08 03:07:27 -0800

    Update battleship

---
+++ battleship/README.md
@@ -0,0 +1,14 @@
+# Battleship
+
+A boringly simple battleship implementation.
+
+## Features
+
+This section is intentionally left blank.
+
+## TODO
+
+* Mobile
+* Multiplayer
+* XR
+* AI
--- battleship/script.js
+++ battleship/script.js
@@ -1,11 +1,15 @@
+// 'use strict' ?
+// TODO disable hover effect at end of game. might remove hover effect entirely, or at least make it *much* more subtle.
+
 class Cell {
-    // element
-    // hasShip
-    // hasShot
+    constructor(element) {
+        this.element = element;
+        this.hasShip = false;
+        this.hasShot = false;
+    }
 }
 
-// TODO gray out area around a ship when it's sunk. Also, update `sunk` value.
-// TODO display for number of ships left.
+let gameOver = false;
 
 class Grid {
     constructor(size = 10) {
@@ -19,6 +23,8 @@ class Ship {
         this.name = name;
         this.size = size;
         this.sunk = false;
+        this.cells = [];
+        this.indices = [];
     }
 }
 
@@ -32,47 +38,143 @@ let ships = [
     new Ship("Destroyer", 2)
 ]
 
-let shotCells = [];
+// TODO put in top right corner, fade/slideout animation on timeout
+// Persist for maybe 3 seconds
+function message(string) {
+    alert(string);
+}
 
 function doShot(event) {
+    if (gameOver) return;
+
     let element = event.currentTarget;
-    console.log(element);
     let id = element.dataset.id;
     if (id === undefined) {
         alert("error")
     }
 
-    console.log(shotCells);
-    if (shotCells.includes(id)) {
+    if (grid.cells[id].hasShot) {
         return;
     }
 
-    var backgroundColor = '#2a2a2a';
-    outer: for (let ship of ships) {
-        for (let index of ship.indices) {
-            if (index == id) {
-                backgroundColor = '#ff0000';
-                break outer;
-            }
+    let didHit = false;
+    let theShip
+    for (let ship of ships) {
+        if (ship.cells[id] !== undefined) {
+            didHit = true;
+            ship.cells[id].hasShot = true;
+            theShip = ship;
+            break;
         }
     }
-    element.style.backgroundColor = backgroundColor;
 
-    let moveCounter = document.querySelector('#moveCounter');
-    moveCounter.innerHTML = Number(moveCounter.innerHTML) + 1;
+    if (didHit) {
+        let sunk = true;
+        for (let cell of theShip.cells) {
+            if (cell === undefined) {
+                continue;
+            }
 
-    shotCells.push(id);
+            if (!cell.hasShot) {
+                sunk = false;
+                break;
+            }
+        }
+        if (sunk) {
+            theShip.sunk = true;
+            let allSunk = true;
+            for (const ship of ships) {
+                if (!ship.sunk) {
+                    allSunk = false;
+                    break;
+                }
+            }
+            if (allSunk) {
+                message("You win!")
+                gameOver = true;
+                let replayButton = document.querySelector('#replayButton');
+                for (let cell of grid.cells) {
+                    cell.element.tabIndex = -1;
+                    // cell.style.pointerEvents = 'none';
+                }
+                replayButton.style.display = 'unset';
+                return;
+            }
+            message(`Sunk the ${theShip.name}!`);
+            let sorted = theShip.indices.sort((a, b) => a - b);
+            let first = sorted[0], last = sorted[sorted.length-1];
+
+            if (theShip.isHorizontal) {
+                for (let i = first - grid.size - 1; i < last - grid.size + 2; i++) {
+                    if (i >= 0 && i < 100) {
+                        grid.cells[i].element.classList.add('miss');
+                    }
+                }
+                for (let i = first + grid.size - 1; i < last + grid.size + 2; i++) {
+                    if (i >= 0 && i < 100) {
+                        grid.cells[i].element.classList.add('miss');
+                    }
+                }
+                if (first - 1 >= 0) grid.cells[first-1].element.classList.add('miss');
+                if (last + 1 < 100) grid.cells[last+1].element.classList.add('miss');
+            } else {
+                for (const index of sorted) {
+                    let indexBefore = index - 1;
+                    let indexAfter = index + 1;
+                    if (indexBefore >= 0 && indexBefore % grid.size != 9) {
+                        grid.cells[indexBefore].element.classList.add('miss');
+                    }
+                    if (indexAfter < 100 && indexAfter % grid.size != 0) {
+                        grid.cells[indexAfter].element.classList.add('miss');
+                    }
+                }
+
+                let indexFirst = first - grid.size;
+                if (indexFirst >= 0) {
+                    grid.cells[indexFirst].element.classList.add('miss');
+                }
+                if (indexFirst - 1 >= 0 && (indexFirst - 1) % grid.size != 9) {
+                    grid.cells[indexFirst - 1].element.classList.add('miss');
+                }
+                if (indexFirst + 1 >= 0 && (indexFirst + 1) % grid.size != 0) {
+                    grid.cells[indexFirst + 1].element.classList.add('miss');
+                }
+
+                let indexLast = last + grid.size;
+                if (indexLast >= 0) {
+                    grid.cells[indexLast].element.classList.add('miss');
+                }
+                if (indexLast - 1 && (indexLast - 1) % grid.size != 9) {
+                    grid.cells[indexLast - 1].element.classList.add('miss');
+                }
+                if (indexLast + 1 && (indexLast + 1) && grid.size != 0) {
+                    grid.cells[indexLast + 1].element.classList.add('miss');
+                }
+            }
+        }
+
+        element.classList.add('hit');
+        let hitCounter = document.querySelector('#hitCounter');
+        hitCounter.innerHTML = Number(hitCounter.innerHTML) + 1;
+    } else {
+        element.classList.add('miss');
+        let missCounter = document.querySelector('#missCounter');
+        missCounter.innerHTML = Number(missCounter.innerHTML) + 1;
+    }
+
+    grid.cells[id].hasShot = true;
 }
 
 function createGrid() {
     const gameBoard = document.getElementById('gameBoard');
 
     for (let i = 0; i < grid.size ** 2; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        cell.dataset.id = i;
-        cell.addEventListener("click", doShot);
-        gameBoard.appendChild(cell);
+        const element = document.createElement('div');
+        element.className = 'cell';
+        element.dataset.id = i;
+        element.addEventListener("click", doShot);
+        gameBoard.appendChild(element);
+        const cell = new Cell(element);
         grid.cells.push(cell);
     }
 }
@@ -80,8 +182,6 @@ function createGrid() {
 function placeShips(grid, ships) {
     let allIndices = [];
     for (let ship of ships) {
-        // What to do about the orientation of a given ship being predictable from the
-        // sunk ships? Math.random < Math.random()? What does that even do?
         outer: while (true) {
             let isHorizontal = Math.random() < 0.5;
             let freeWidth = isHorizontal ? grid.size - ship.size : grid.size;
@@ -102,8 +202,13 @@ function placeShips(grid, ships) {
                     }
                 }
                 indices.push(index);
+                ship.isHorizontal = isHorizontal;
             }
-            indices.forEach(i => allIndices.push(i));
+            for (const index of indices) {
+                allIndices.push(index);
+                grid.cells[index].hasShip = true;
+                ship.cells[index] = grid.cells[index];
+            }
             ship.indices = indices;
             break;
         }
@@ -113,6 +218,16 @@ function placeShips(grid, ships) {
 function startGame() {
     createGrid();
     placeShips(grid, ships);
+<<<<<<< HEAD
+=======
+    message("5 ships remaining");
+
+    // for (let ship of ships) {
+    //     for (let index of ship.indices) {
+    //         grid.cells[index].element.innerHTML = ship.size;
+    //     }
+    // }
+>>>>>>> af0b7de (Updated battleship)
 }
 
 document.addEventListener('DOMContentLoaded', startGame);
